845bbd1d54912f5d3655a7b134fa71c2312f02d0
Rene Hexel <r.hexel@griffith.edu.au>
2016-06-13 16:25:23 +1000

    Handle -Wl, and -pthread linker flags

--- config.sh
+++ config.sh
@@ -7,5 +7,5 @@ Mod=GModule
 Module=${Mod}-2.0
 module=`echo "${Module}" | tr '[:upper:]' '[:lower:]'`
 export PATH=/Library/Developer/Toolchains/swift-latest.xctoolchain/usr/bin:"${PATH}"
-LINKFLAGS=`pkg-config --libs $module gio-unix-2.0 glib-2.0 | tr ' ' '\n' | sed 's/^/-Xlinker /' | tr '\n' ' '`
+LINKFLAGS=`pkg-config --libs $module gio-unix-2.0 glib-2.0 | tr ' ' '\n' | sed -e 's/^/-Xlinker /' -e 's/-Wl,//g' -e 's/ -pthread/ -lpthread/g' | tr '\n' ' '`
 CCFLAGS=`pkg-config --cflags $module gio-unix-2.0 glib-2.0 | tr ' ' '\n' | sed 's/^/-Xcc /' | tr '\n' ' ' `
