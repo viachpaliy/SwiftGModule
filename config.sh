f82933e0b56762284bc21d1f8a3eaa8df478c866
Rene Hexel <r.hexel@griffith.edu.au>
2016-07-31 13:58:10 +1000

    Compile with Swift 3.0 preview 3

--- config.sh
+++ config.sh
@@ -6,6 +6,6 @@
 Mod=GModule
 Module=${Mod}-2.0
 module=`echo "${Module}" | tr '[:upper:]' '[:lower:]'`
-export PATH=/Library/Developer/Toolchains/swift-latest.xctoolchain/usr/bin:"${PATH}"
+export PATH=`pwd`/.build/gir2swift/.build/debug:"${PATH}"
 LINKFLAGS=`pkg-config --libs $module gio-unix-2.0 glib-2.0 | tr ' ' '\n' | sed -e 's/^/-Xlinker /' -e 's/-Wl,//g' -e 's/ -pthread/ -lpthread/g' | tr '\n' ' '`
 CCFLAGS=`pkg-config --cflags $module gio-unix-2.0 glib-2.0 | tr ' ' '\n' | sed 's/^/-Xcc /' | tr '\n' ' ' `
