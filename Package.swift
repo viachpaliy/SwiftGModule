28ce26e9073dc2d8a063be8779d4c23dc26145ed
Rene Hexel <r.hexel@griffith.edu.au>
2017-09-28 06:34:05 +1000

    Update to Swift 4

--- Package.swift
+++ Package.swift
@@ -4,5 +4,6 @@ let package = Package(
     name: "GModule",
     dependencies: [
         .Package(url: "https://github.com/rhx/SwiftGLib.git", majorVersion: 2)
-    ]
+    ],
+    swiftLanguageVersions: [3, 4]
 )
